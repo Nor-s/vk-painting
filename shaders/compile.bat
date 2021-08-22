ae563cfd406d0f1e08aff886f73678ca3eabf4f7
Nor-s <s28118149@gmail.com>
2021-08-22 21:27:56 +0900

    Init commit

---
+++ shaders/compile.bat
@@ -0,0 +1,4 @@
+C:\VulkanSDK\1.2.182.0\Bin32\glslc.exe fs_default.frag -o fs_default.spv
+C:\VulkanSDK\1.2.182.0\Bin32\glslc.exe vs_default.vert -o vs_default.spv
+
+pause
---
+++ src/class/application.h
@@ -0,0 +1,34 @@
+#ifndef _APPLICATION_H
+#define _APPLICATION_H
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "stb/stb_image.h"
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <chrono>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <array>
+#include <optional>
+#include <set>
+
+namespace Painting
+{
+    class Application
+    {
+    public:
+        void run();
+    };
+}
+#endif
