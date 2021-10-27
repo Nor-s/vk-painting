8bc84b9b0bb82f0962c1bece4bd8b00cd8c7f632
Nor-s <s28118149@gmail.com>
2021-10-28 03:04:05 +0900

    Add precompiled header

---
+++ src/vkcpp/stdafx.h
@@ -0,0 +1,29 @@
+#ifndef VKCPP_STDAFX_H
+#define VKCPP_STDAFX_H
+
+#include <chrono>
+#include <algorithm>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <memory>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+#include <cstring>
+#include <string>
+#include <thread>
+#include <cstdint>
+
+#include <set>
+#include <mutex>
+#include <assert.h>
+#include <optional>
+#include <fstream>
+#include <array>
+#include <utility>
+#include <cstdlib>
+
+#endif
